1f0c10393da1a2383d5a09095fd888a738fdc9da
lucas villefort <lucasvillefort11@gmail.com>
2024-07-13 19:22:31 -0300

    sd

--- Python Django - The Practical Guide/1-module/my_site/my_site/settings.py
+++ Python Django - The Practical Guide/1-module/my_site/my_site/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "my_site.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
+        "DIRS": [BASE_DIR / "blog" / "static"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,7 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]  # i need to put this path over here too
+STATICFILES_DIRS = [
+    BASE_DIR / "blog" / "static"
+]  # i need to put this path over here too
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
