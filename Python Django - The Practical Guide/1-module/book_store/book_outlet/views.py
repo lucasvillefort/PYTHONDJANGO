6ee708b279893b3d3a2b7dfc95d1052c66041141
lucas villefort <lucasvillefort11@gmail.com>
2024-07-14 22:24:42 -0300

    sd

--- Python Django - The Practical Guide/1-module/book_store/book_outlet/views.py
+++ Python Django - The Practical Guide/1-module/book_store/book_outlet/views.py
@@ -1,3 +1,29 @@
+from ast import Try
+from django.http import Http404
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
+
+
+def index(request):
+    # Book.objects.filter()
+    books = Book.objects.all()
+    return render(request, "index.html", {"books": books})
+
+
+def book_detail(request, id):
+    try:
+        book = Book.objects.get(pk=id)  # primary key
+        return render(
+            request,
+            "book_detail.html",
+            {
+                "title": book.title,
+                "author": book.author,
+                "rating": book.rating,
+                "is_bestseller": book.is_bestselling,
+            },
+        )
+    except:
+        raise Http404()
